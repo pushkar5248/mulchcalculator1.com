4b9b05112a8881ef30714cb3c2c7ec1cf7806114
pushkar5248 <pushkargoyal5248@gmail.com>
2026-06-13 00:03:31 +0530

    added sitemap.xml

--- super-shepherd/astro.config.mjs
+++ super-shepherd/astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://mulchcalculator1.com',
+  integrations: [react(), sitemap()],
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "es", "fr", "hi", "ru", "it"],
+    locales: ["en", "es", "fr", "hi", "ru", "it", "de"],
     routing: {
       prefixDefaultLocale: false,
     },
