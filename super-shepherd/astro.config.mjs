aa267758dd65b99b8e72881b40dfc29744141a6d
pushkar5248 <pushkargoyal5248@gmail.com>
2026-06-13 20:10:42 +0530

    fixed robots.txt

--- super-shepherd/astro.config.mjs
+++ super-shepherd/astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mulchcalculator1.com',
   integrations: [react(), sitemap()],
+  output: 'server',
+  adapter: cloudflare(),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es", "fr", "hi", "ru", "it", "de"],
